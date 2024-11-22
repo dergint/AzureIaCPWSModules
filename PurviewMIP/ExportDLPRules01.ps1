d8496eb8d26e6430acec3142806252e5bb0afbc3
dergint <129293943+dergint@users.noreply.github.com>
2024-11-22 11:55:47 +0000

    UPdated exportdlp script

--- PurviewMIP/ExportDLPRules01.ps1
+++ PurviewMIP/ExportDLPRules01.ps1
@@ -33,7 +33,8 @@ if ($confirmation -ne "OK") {
 }
 
 try {
-    Connect-IPPSSession
+    Connect-IPPSSession 
+    Write-Output "Connection to Purview SCC successful."
 }
 catch {
     Write-Error "Failed to connect to Purview SCC. Error details: $($_.Exception.Message)"
@@ -44,24 +45,46 @@ catch {
 try {
     Write-Output "Getting DLP Policy Rules..."
     $skydlprulesexport = Get-DlpComplianceRule
+    $skydlppolicies = Get-DlpCompliancePolicy
+    $skylabelpolicies = Get-LabelPolicy
+    $skylabels = Get-Label
 }
 catch {
     Write-Error "Failed to get DLP Policy Rules. Error details: $($_.Exception.Message)"
     Exit
 }
 
-$SaveConfigFilePath = "$ConfigFolderPath\SkyDlpRuleExport_$Date.json"
-$SaveReportFilePath = "$ReportFolderPath\SkyDLPRules.json"
+$SaveDLPRuleFilePath = "$ConfigFolderPath\SkyDlpRuleExport_$Date.json"
+$SaveDLPRuleReportFilePath = "$ReportFolderPath\SkyDLPRules.json"
+$SaveDLPPolicyFilePath = "$ConfigFolderPath\SkyDlpPolicyExport_$Date.json"
+$SaveDLPPolicyReportFilePath = "$ReportFolderPath\SkyDlpPolicy.json"
+$SaveLabelPolicyFilePath = "$ConfigFolderPath\SkyLabelPolicy_$Date.json"
+$SaveLabelPolicyReportFilePath = "$ReportFolderPath\SkyLabelPolicy.json"
+$SaveLabelsFilePath = "$ConfigFolderPath\SkyLabels_$Date.json"
+$SaveLabelsReportFilePath = "$ReportFolderPath\SkyLabels.json"
 
 # Export JSON files
-$skydlprulesexport | ConvertTo-Json -Depth 100 | Out-File -Encoding UTF8 -FilePath $SaveConfigFilePath
-$skydlprulesexport | Select-Object DisplayName, ParentPolicyName, ReportSeverityLevel, BlockAccess, GenerateAlert, Disabled, Mode | ConvertTo-Json -Depth 100 | Out-File -Encoding UTF8 -FilePath $SaveReportFilePath
+$skydlprulesexport | ConvertTo-Json -Depth 100 | Out-File -Encoding UTF8 -FilePath $SaveDLPRuleFilePath
+$skydlprulesexport | Select-Object DisplayName, ParentPolicyName, ReportSeverityLevel, BlockAccess, GenerateAlert, Disabled, Mode | ConvertTo-Json -Depth 100 | Out-File -Encoding UTF8 -FilePath $SaveDLPRuleReportFilePath
+$skydlppolicies | ConvertTo-Json -Depth 100 | Out-File -Encoding UTF8 -FilePath $SaveDLPPolicyFilePath
+$skydlppolicies | Select-Object Name, Mode, IsSimulationPolicy | ConvertTo-Json -Depth 100 | Out-File -Encoding UTF8 -FilePath $SaveDLPPolicyReportFilePath
+$skylabelpolicies | ConvertTo-Json -Depth 100 | Out-File -Encoding UTF8 -FilePath $SaveLabelPolicyFilePath
+$skylabelpolicies | Select-Object Name, Labels, Settings, Workload, Priority, Enabled | ConvertTo-Json -Depth 100 | Out-File -Encoding UTF8 -FilePath $SaveLabelPolicyReportFilePath
+$skylabels | ConvertTo-Json -Depth 100 | Out-File -Encoding UTF8 -FilePath $SaveLabelsFilePath
+$skylabels | Select-Object Name, ContentType, Priority, DisplayName | ConvertTo-Json -Depth 100 | Out-File -Encoding UTF8 -FilePath $SaveLabelsReportFilePath
+
 
 Write-Output "DLP Rule Export Completed. Files saved to:"
-Write-Output $SaveConfigFilePath
-Write-Output $SaveReportFilePath
+Write-Output $SaveDLPRuleFilePath
+Write-Output $SaveDLPRuleReportFilePath
+Write-Output $SaveDLPPolicyFilePath
+Write-Output $SaveDLPPolicyReportFilePath
+Write-Output $SaveLabelPolicyFilePath 
+Write-Output $SaveLabelPolicyReportFilePath
+Write-Output $SaveLabelsFilePath
+Write-Output $SaveLabelsReportFilePath
 
-# Part 2 - Script will now produce a delta file and compare the changes from previous configuration File
+<# Part 2 - Script will now produce a delta file and compare the changes from previous configuration File
 Write-Host "Do you want to compare the latest config to previous one? Type 'OK' to proceed or press Enter to cancel."
 $confirmationForCompare = Read-Host
 
@@ -103,4 +126,5 @@ $ConfigDifferences = Compare-Object -ReferenceObject $PreviousDLPConfig -Differe
 $ConfigDifferenceFilePath = "$ReportFolderPath\DlpConfigChanges.json"
 $ConfigDifferences | ConvertTo-Json -Depth 100 | Out-File -Encoding UTF8 -FilePath $ConfigDifferenceFilePath 
 
-Write-Host "Config Difference saved to $ConfigDifferenceFilePath"
+Write-Host "Config Difference saved to $ConfigDifferenceFilePath" 
+#>
