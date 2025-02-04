0860bc0bb6f21a09f3193066e0ba05013106a527
dergint <129293943+dergint@users.noreply.github.com>
2025-02-04 17:41:21 +0000

    update latest files

--- EntraID/InactiveUsers_by_Usertype.ps1
+++ EntraID/InactiveUsers_by_Usertype.ps1
@@ -8,21 +8,29 @@ if (!(Get-MgContext)) {
     Connect-MgGraph -Scopes "AuditLog.Read.All", "User.Read.All"
 }
 
-# Get current date
+# Get the cutoff date (30 days ago)
 $CutoffDate = (Get-Date).AddDays(-90)
 
-# Get all users with sign-in activity
-$Users = Get-MgUser -All -Property DisplayName,UserPrincipalName,SignInActivity | Select-Object DisplayName,UserPrincipalName,UserType,SignInActivity
+# Get all users with last sign-in details
+$Users = Get-MgUser -All -Property DisplayName, UserPrincipalName, SignInActivity
+
+# Fetch the user details including 'UserType' separately
+$UserDetails = Get-MgUser -All -Property DisplayName, UserPrincipalName, UserType | 
+Select-Object DisplayName, UserPrincipalName, UserType
+
+# Join both datasets based on UserPrincipalName
+$UserDict = @{}
+$UserDetails | ForEach-Object { $UserDict[$_.UserPrincipalName] = $_.UserType }
 
 # Filter users who have not signed in within the last 30 days
 $InactiveUsers = $Users | Where-Object {
-    ($_.LastSignInDateTime -eq $null) -or ($_.LastSignInDateTime -lt $CutoffDate)
+    ($_.SignInActivity.LastSignInDateTime -eq $null) -or ($_.SignInActivity.LastSignInDateTime -lt $CutoffDate)
 } | ForEach-Object {
     [PSCustomObject]@{
         DisplayName        = $_.DisplayName
         UserPrincipalName  = $_.UserPrincipalName
-        UserType           = $_.UserType  # Adding UserType (Member/Guest)
-        LastSignInDateTime = $_.LastSignInDateTime
+        UserType           = $UserDict[$_.UserPrincipalName]  # Retrieve UserType
+        LastSignInDateTime = $_.SignInActivity.LastSignInDateTime
     }
 }
 
